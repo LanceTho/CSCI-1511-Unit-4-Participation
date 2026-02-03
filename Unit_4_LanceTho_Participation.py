8136f348281957a8cb7e9f19d829caf3716085af
Lance Thongsavanh <lancer8106@gmail.com>
2026-02-03 13:57:13 -0500

    Prints out the total number of elements. Prints out the sum. Calulates the average. Prints the average.

--- Unit_4_LanceTho_Participation.py
+++ Unit_4_LanceTho_Participation.py
@@ -1,5 +1,5 @@
 """
-Use list comprehension to create a list of random numbers from 1-10
+Use list comprehension to create a list of 100 random numbers from 1-10
 list_example = 10, 9, 8, 1, 10, 9, 4, etc.
 
 Print out the total elements in the list
@@ -9,4 +9,8 @@ Print out the average
 import random
 
 random_numbers = [random.randint(1, 10) for _ in range(100)]
-print(random_numbers)
+print(f"Here is a randomly generated list of numbers from 1-10: {random_numbers}")
+print(f"Here is the total elements in the list: {len(random_numbers)}")
+print(f"Here is the sum: {sum(random_numbers)}")
+average: float = sum(random_numbers) / len(random_numbers)
+print(f"Here is the average: {average}")
