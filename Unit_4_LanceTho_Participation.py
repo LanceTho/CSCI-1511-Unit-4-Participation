8abde7b697f86bafc310a234bfc7fee501976d5c
Lance Thongsavanh <147730520+LanceTho@users.noreply.github.com>
2026-02-03 13:40:16 -0500

    Create Unit_4_LanceTho_Participation.py

---
+++ Unit_4_LanceTho_Participation.py
@@ -0,0 +1,8 @@
+"""
+Use list comprehension to create a list of random numbers from 1-10
+list_example = 10, 9, 8, 1, 10, 9, 4, etc.
+
+Print out the total elements in the list
+Print out the sum
+Print out the average
+"""
