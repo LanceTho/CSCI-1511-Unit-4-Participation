3cee2335470553a6230b176102d299a6cbb64239
Lance Thongsavanh <lancer8106@gmail.com>
2026-02-03 13:50:25 -0500

    Imported random. Creates a list of 100 random numbers from 1-10 using list comprehension and prints the list.

--- Unit_4_LanceTho_Participation.py
+++ Unit_4_LanceTho_Participation.py
@@ -6,3 +6,7 @@ Print out the total elements in the list
 Print out the sum
 Print out the average
 """
+import random
+
+random_numbers = [random.randint(1, 10) for _ in range(100)]
+print(random_numbers)
